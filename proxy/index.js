c77610c300ef62a397fd135d1784d03743ab0909
Lilly Tang <lillytang@Lillys-MacBook-Pro.local>
2020-01-08 14:21:56 -0800

    updated my proxy request to target the gallery microservice hosted on ec2

--- proxy/index.js
+++ proxy/index.js
@@ -4,11 +4,10 @@ const app = express();
 const path = require("path");
 const port = 8080;
 const proxy = require("http-proxy-middleware");
+app.use("/*", express.static(path.resolve(__dirname, "../public")));
 
-app.use("/", express.static(path.resolve(__dirname, "../public")));
-
-// proxy request to localhost:3004 that serves the gallery microservice 
-app.use('/airbnb/listings/:id', proxy({ target: 'http://localhost:3004', changeOrigin: true }))
+// proxy request to the gallery service hosted on ec2
+app.use('/airbnb/listings/:id', proxy({ target: 'http://54.177.59.24:3008/', changeOrigin: true }))
 
 app.listen(port, () => {
   console.log(`listening to port ${port}...`);
